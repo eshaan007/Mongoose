0a4531b0d0670a0895aca2ad67a0bc2ae0716618
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-17 23:00:42 +0530

    Mongoose part 2

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Pizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
